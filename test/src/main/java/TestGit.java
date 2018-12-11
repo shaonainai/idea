da940d6b2726514e5253fa33ed208cac6a33b2d5
shaonainai <45782482+shaonainai@users.noreply.github.com>
2018-12-11 19:31:35 +0800

    update 5

--- test/src/main/java/TestGit.java
+++ test/src/main/java/TestGit.java
@@ -3,6 +3,8 @@ public class TestGit {
         System.out.println("11111111");
         System.out.println("22222222");
         System.out.println("33333333");
+        System.out.println("44444444");
         System.out.println("args = " + args);
+
     }
 }
