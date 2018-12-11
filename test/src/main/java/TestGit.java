dcd3318c4e273ce2cc85683e83a75b7d4fb02bef
xiaonaiba <atguigu_b@163.com>
2018-12-11 19:06:22 +0800

    insert 4

--- test/src/main/java/TestGit.java
+++ test/src/main/java/TestGit.java
@@ -3,5 +3,6 @@ public class TestGit {
         System.out.println("11111111");
         System.out.println("22222222");
         System.out.println("33333333");
+        System.out.println("args = " + args);
     }
 }
